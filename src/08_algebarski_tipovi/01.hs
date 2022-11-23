75c07bc7212d368d7be20962e351a4b3add02dbe
Ivan <52036829+blin04@users.noreply.github.com>
2022-11-23 11:30:46 +0000

    dodat prvi zadatak iz oblasti 08

---
+++ src/08_algebarski_tipovi/01.hs
@@ -0,0 +1,24 @@
+data Vektor = Vektor Float Float Float deriving (Show)
+
+-- sabiranje vektora
+saberi::Vektor->Vektor->Vektor
+saberi (Vektor x1 y1 z1) (Vektor x2 y2 z2) = Vektor (x1 + x2) (y1 + y2) (z1 + z2)
+
+-- mnozenje vektora skalarem
+skaliraj::Vektor->Float->Vektor
+skaliraj (Vektor x1 y1 z1) k = Vektor (k*x1) (k*y1) (k*z1)
+
+-- skalarni proizvod vektora
+skalarniProizvod::Vektor->Vektor->Float
+skalarniProizvod (Vektor x1 y1 z1) (Vektor x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
+
+-- vektorski proizvod vektora
+vektorskiProizvod::Vektor->Vektor->Vektor
+vektorskiProizvod (Vektor x1 y1 z1) (Vektor x2 y2 z2) = Vektor a b c
+	where a = (y1*z2 - z1*y2)
+	      b = -1 * (x1*z2 - z1*x2)
+	      c = (x1*y2 - x2*y1)
+
+-- duzina vektora
+duzina::Vektor->Float
+duzina (Vektor x y z) = sqrt(x*x + y*y + z*z)
