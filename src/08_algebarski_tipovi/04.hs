2a1ac13026a625877a3df1600447cfbe9ce0075a
Ivan <52036829+blin04@users.noreply.github.com>
2022-11-23 21:27:19 +0000

    obrisani komentari

--- src/08_algebarski_tipovi/04.hs
+++ src/08_algebarski_tipovi/04.hs
@@ -1,18 +1,14 @@
 data KompleksanBroj = Kompleksan Float Float deriving(Show)
 
--- sabiranje
 saberi::KompleksanBroj->KompleksanBroj->KompleksanBroj
 saberi (Kompleksan a b) (Kompleksan c d) = Kompleksan (a + c) (b + d)
 
--- oduzimanje
 oduzmi::KompleksanBroj->KompleksanBroj->KompleksanBroj
 oduzmi (Kompleksan a b) (Kompleksan c d) = Kompleksan (a - c) (b - d)
 
--- mnozenje
 pomnozi::KompleksanBroj->KompleksanBroj->KompleksanBroj
 pomnozi (Kompleksan a b) (Kompleksan c d) = Kompleksan (a * c - b * d) (a * d + b * c)
 
--- deljenje
 podeli::KompleksanBroj->KompleksanBroj->KompleksanBroj
 podeli (Kompleksan a b) (Kompleksan c d) = Kompleksan ((a * c + b * d) / temp) ((b * c - a * d) / temp)
         where temp = c * c + d * d
